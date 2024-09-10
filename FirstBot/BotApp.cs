b8a8d2987b032288a81394040f11c5f6f958f780
OZZY <Zyadhimself1@gmail.com>
2024-09-10 22:46:15 +0300

    Update BotApp.cs

--- FirstBot/BotApp.cs
+++ FirstBot/BotApp.cs
@@ -14,7 +14,7 @@ namespace FirstBot
     {
         public static readonly DiscordSocketClient _client;
 
-        internal const string _token = @"MTI4Mjk5Mzg2MTU2Nzk3MTMyOA.GhWaUn.lxRXrE7196OpQi03cgN1bsSv9ggNqRjrHfBPWI";
+        internal const string _token = @"MTI4Mjk5Mzg2MTU2Nzk3MTMyOA.GhWaUn.lxRXrE7196OpQi03cgN1bsSv9ggNqRjrHfBPWI"; // Check that token out bro 
 
         static BotApp()
         {
