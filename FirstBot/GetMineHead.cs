99a6f153fafd1a3e536e7ca0db27fabf2eb0f244
Ozzy-ZY <Zyadhimself1@gmail.com>
2024-09-10 15:12:43 +0300

    Add project files.

---
+++ FirstBot/GetMineHead.cs
@@ -0,0 +1,29 @@
+﻿using Discord.WebSocket;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FirstBot
+{
+    public class GetMineHead
+    {
+        private static HttpClient _http = new HttpClient();
+
+        public static async Task<string> GetMinecraftAvatarAsync(string message)
+        {
+            //var str = await _http.GetStringAsync("https://evilinsult.com/generate_insult.php?lang=en&type=text");
+            string path = "C:\\Users\\user\\source\\repos\\FirstBot\\FirstBot\\Assets\\temp.png";
+            var img = await _http.GetAsync($"https://minotar.net/helm/{message}/100.png");
+            img.EnsureSuccessStatusCode();
+            await using var fileStream = new FileStream
+                (path, FileMode.Create);
+            await img.Content.CopyToAsync(fileStream);
+            Console.WriteLine("Image Downloading");
+            return path;
+            //return str;
+        }
+    }
+}
---
+++ FirstBot/Program.cs
@@ -0,0 +1,140 @@
+﻿using Discord;
+using Discord.Net;
+using Discord.WebSocket;
+using Newtonsoft.Json;
+using System.Reactive.Subjects;
+
+namespace FirstBot
+{
+    internal class Program
+    {
+        private readonly DiscordSocketClient _client;
+
+        private const string _token = @"MTI4Mjk5Mzg2MTU2Nzk3MTMyOA.GhWaUn.lxRXrE7196OpQi03cgN1bsSv9ggNqRjrHfBPWI";
+
+        public Program()
+        {
+            var Config = new DiscordSocketConfig()
+            {
+                GatewayIntents = GatewayIntents.All
+            };
+            this._client = new DiscordSocketClient(Config);
+            _client.MessageReceived += MessageHandler;
+            _client.Ready += CreateSlashCommand;
+            _client.SlashCommandExecuted += SlashCommandHandler;
+
+            Console.WriteLine("INIT Program");
+        }
+
+        public async Task StartBotAsync()
+        {
+            _client.Log += LogAsync;
+            await _client.LoginAsync(Discord.TokenType.Bot, _token);
+            await _client.StartAsync();
+            //_client.Ready += RegisterCommandsAsync;
+            await Task.Delay(-1);
+            Console.WriteLine("Started Bot Async indifnitly");
+        }
+
+        private async Task MessageHandler(SocketMessage message)
+        {
+            Console.WriteLine("started Message Handler");
+            if (message.Author.IsBot)
+                return;
+            var insult = await GetMineHead.GetMinecraftAvatarAsync(message.Content);
+            await ReplyAsync(message, insult);
+        }
+
+        private async Task ReplyAsync(SocketMessage message, string response)
+        {
+            Console.WriteLine("replying Async");
+            await message.Channel.SendMessageAsync(response);
+            await message.Channel.SendFileAsync(response);
+        }
+
+        private async Task LogAsync(LogMessage log)
+        {
+            await Task.Run(() => Console.WriteLine(log));
+        }
+
+        public async Task CreateSlashCommand()
+        {
+            var guild = _client.GetGuild(481968987929051167);
+            var guildCommand = new SlashCommandBuilder()
+                .WithName("minehead")
+                .WithDescription("Gets the minecraft head of specified user")
+                .AddOption("username", ApplicationCommandOptionType.String,
+                "The username of the minecraft account", isRequired: true);
+            try
+            {
+                await guild.CreateApplicationCommandAsync(guildCommand.Build());
+            }
+            catch (HttpException e)
+            {
+                var json = JsonConvert.SerializeObject(e.Errors, Formatting.Indented);
+                Console.WriteLine(json);
+            }
+        }
+
+        //private async Task SlashCommandHandler(SocketSlashCommand command)
+        //{
+        //    await command.RespondAsync($"You executed {command.Data.Name}");
+        //}
+
+        private async Task SlashCommandHandler(SocketSlashCommand command)
+        {
+            // Let's add a switch statement for the command name so we can handle multiple commands in one event.
+            switch (command.Data.Name)
+            {
+                case "minehead":
+                    await HandleMineHeadCommand(command);
+                    break;
+            }
+        }
+
+        private async Task HandleMineHeadCommand(SocketSlashCommand command)
+        {
+            var name = command.Data.Options.First().Value;
+            var imgPath = await GetMineHead.GetMinecraftAvatarAsync(name as string ?? "zyad");
+            await command.RespondWithFileAsync(imgPath);
+        }
+
+        // bot Id : 1282993861567971328
+        // GUILD Id : 481968987929051167
+
+        //private async Task RegisterCommandsAsync()
+        //{
+        //    var guild = _client.GetGuild(481968987929051167);
+
+        //    var testCommand = new SlashCommandBuilder()
+        //        .WithName("hello")
+        //        .WithDescription("Replies with Hello!")
+        //        .Build();
+
+        //    await guild.CreateApplicationCommandAsync(testCommand);
+
+        //    Console.WriteLine("Slash commands registered.");
+        //}
+
+        //private async Task SlashCommandHandler(SocketSlashCommand command)
+        //{
+        //    Console.WriteLine("command Executing");
+        //    switch (command.Data.Name)
+        //    {
+        //        case "hello":
+        //            await command.RespondAsync("Hello!");
+        //            break;
+
+        //        default:
+        //            await command.RespondAsync("Unknown command.");
+        //            break;
+        //    }
+        //}
+
+        private static async Task Main(string[] args)
+        {
+            var mybot = new Program();
+            await mybot.StartBotAsync();
+        }
+    }
+}
